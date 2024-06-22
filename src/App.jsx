f36dc585cb640e0c08823adfbbbe42337ef10216
AndriiKot <a_kocyuba@ukr.net>
2024-06-23 00:05:26 +0200

    React 'React_03' ! fetch testing 1 !

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [posts, setPosts] = useState([]);
+
+  const fetchPosts = () => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => setPosts(json));
+  };
+
+  useEffect(() => {
+    console.log("fetchPosts");
+    fetchPosts();
+  }, []);
+
+  console.log("HI!!!");
+
+  return (
+    <div>
+      {posts.map((post) => (
+        <div key={post.id}>{post.title}</div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
