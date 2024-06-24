41b7f345dc301802568cbcbe0d0d2c6e60a0acda
AndriiKot <a_kocyuba@ukr.net>
2024-06-24 12:37:22 +0200

    React 'React_03' new branch 'NewFunctional_2' ! not list TESCTING testing 1 !

--- src/Card/CardsList.jsx
+++ src/Card/CardsList.jsx
@@ -2,6 +2,12 @@
 
 export default function CardsList(props) {
   const { list } = props;
+  if (!list) {
+    console.log("HOt list");
+  } else {
+    console.log("Cold list");
+  }
+
   return list.map((post) => (
     <Card
       key={post.id}
