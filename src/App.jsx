8e79c5b0c3bf9894a12fdcbf7dda9cd5de2c8b6f
AndriiKot <a_kocyuba@ukr.net>
2024-06-24 12:38:39 +0200

    React 'React_03' new branch 'NewFunctional_2' ! not list TESCTING testing 2 !

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   }, []);
   return (
     <div className="cards-container">
-      <CardsList list={posts} />
+      <CardsList  />
     </div>
   );
 }
--- src/Card/CardsList.jsx
+++ src/Card/CardsList.jsx
@@ -3,10 +3,8 @@
 export default function CardsList(props) {
   const { list } = props;
   if (!list) {
-    console.log("HOt list");
-  } else {
-    console.log("Cold list");
-  }
+    return console.log("HOt list");
+  } 
 
   return list.map((post) => (
     <Card
