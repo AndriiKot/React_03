27ff195d1cd409f5e2d71e712129d57d4bbb49ec
AndriiKot <a_kocyuba@ukr.net>
2024-06-24 19:28:53 +0200

    React 'React_03'  ! CREATE 'SPLIT FETCH' !

--- src/FetchPosts/FetchPosts.jsx
+++ src/FetchPosts/FetchPosts.jsx
@@ -1,3 +1,21 @@
-﻿export default function FetchPosts() {
-  console.log("Fetching posts...");
+﻿import { useEffect, useState } from "react";
+
+export default function FetchPosts() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchPosts = async () => {
+    return fetch("https://jsonplaceholder.typicode.com/posts").then(
+      (response) => response.json()
+    );
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    fetchPosts()
+      .then((data) => setPosts(data))
+      .then(() => setLoading(false));
+  }, []);
+
+  return [posts, loading];
 }
